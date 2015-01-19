07c52f353f22ffd1bcd209e58afe0b278c8266e6
kqueryful <kqueryful@gmail.com>
2015-01-18 23:43:28 -0800

    Sanseido Definitions
    Add Daily Concise J-J definitions to Anki words

---
+++ Sanseido Definitions/sanseidoDefsForAnki.py
@@ -0,0 +1,91 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+	Sanseido Definitions plugin for Anki
+	pulls definitions from sanseido.net's デイリーコンサイス国語辞典
+	
+	Definition fetching adapted from rikaichan.js
+	Field updating modified from Sentence_Gloss.py
+	
+	@author 	= kqueryful
+	@date 		= 1/18/2015
+	@version 	= 1.0
+"""
+from bs4.BeautifulSoup import BeautifulSoup
+import urllib
+import re
+
+# Edit these field names if necessary ==========================================
+expressionField = 'Word'
+definitionField = 'Sanseido'
+# ==============================================================================
+
+# Fetch definition from Sanseido ===============================================
+def fetchDef(term):
+	defText = ""
+	pageUrl = "http://www.sanseido.net/User/Dic/Index.aspx?TWords=" + urllib.quote(term.encode('utf-8')) + "&st=0&DailyJJ=checkbox"
+	response = urllib.urlopen(pageUrl)
+	soup = BeautifulSoup(response)
+	NetDicBody = soup.find('div', class_ = "NetDicBody")
+
+	if NetDicBody != None:
+		defFinished = False
+		
+		for line in NetDicBody.children:
+			if line.name == "b":
+				if len(line) != 1:
+					for child in line.children:
+						if child.name == "span":
+							defFinished = True
+			if defFinished:
+				break
+			
+			if line.string != None and line.string != u"\n":
+				defText += line.string
+				
+	return re.sub(ur"[^#]（(?P<num>[２-９]+)）", ur"<br/>（\1）", defText)
+
+# Update note ==================================================================
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from anki.hooks import addHook
+from anki.notes import Note
+from aqt import mw
+
+def glossNote( f ):
+   if f[ definitionField ]: return
+   f[ definitionField ] = fetchDef( f[ expressionField ] )
+
+def setupMenu( ed ):
+	a = QAction( 'Regenerate Sanseido definitions', ed )
+	ed.connect( a, SIGNAL('triggered()'), lambda e=ed: onRegenGlosses( e ) )
+	ed.form.menuEdit.addAction( a )
+
+def onRegenGlosses( ed ):
+	n = "Regenerate Sanseido definitions"
+	ed.editor.saveNow()
+	regenGlosses(ed, ed.selectedNotes() )   
+	mw.requireReset()
+	
+def regenGlosses( ed, fids ):
+	mw.progress.start( max=len( fids ) , immediate=True)
+	for (i,fid) in enumerate( fids ):
+		mw.progress.update( label='Generating Sanseido definitions...', value=i )
+		f = mw.col.getNote(id=fid)
+		try: glossNote( f )
+		except:
+			import traceback
+			print 'definitions failed:'
+			traceback.print_exc()
+		try: f.flush()
+		except:
+			raise Exception()
+		ed.onRowChanged(f,f)
+	mw.progress.finish()
+	
+addHook( 'browser.setupMenus', setupMenu )
+
+
+
+
